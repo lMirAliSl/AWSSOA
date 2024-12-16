cb247f5d66b12c14887fe5dfa5805a3cbfc54023
Oleksandr Nikolaichuk <71693664+lMirAliSl@users.noreply.github.com>
2024-12-16 23:20:38 +0200

    LAB4

--- BudgetService/api.py
+++ BudgetService/api.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calculation.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask_user:Nikolas2815@userdb.c54yg0ggumzr.eu-central-1.rds.amazonaws.com:5432/user_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Модель для бюджету
