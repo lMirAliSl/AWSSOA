73f5e3ce752d5f4c5de016f197983d551be5e0a3
Oleksandr Nikolaichuk <santifick.nik@gmail.com>
2024-12-17 00:41:13 +0200

    LAB4

--- SIte/website/__init__.py
+++ SIte/website/__init__.py
@@ -41,7 +41,7 @@ def create_app():
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(user_id):
-        return UserModel.query.get(int(user_id))
+    def load_user(id):
+        return UserModel.query.get(int(id))
 
     return app
